223b210c13188febfb98f46038b35eebd4b89195
Matthew Alan Gray <matt.gray@american-equity.com>
2021-12-04 05:50:36 -0600

    More tweaks

--- Hands-on lab/lab-files/developer/scripts/create_build_environment.sh
+++ Hands-on lab/lab-files/developer/scripts/create_build_environment.sh
@@ -55,14 +55,3 @@ if [[ ! -e ~/Fabmedical ]]; then
     git config --global http.$MCW_GITHUB_URL.extraHeader "Authorization: Basic $GIT_AUTH"
     git clone https://github.com/$MCW_GITHUB_USERNAME/Fabmedical ~/Fabmedical
 fi
-
-# Set up docker environment for mongodb backend
-docker network create fabmedical
-docker container run --name mongo --net fabmedical -p 27017:27017 -d mongo:4.0
-
-# Seed the mongo db
-cd ~/Fabmedical/content-init
-npm ci
-nodejs server.js
-
-echo "Build agent setup complete!"
